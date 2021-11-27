a80423077087ee44304e22053e70d0a6d82daa81
David Komar <komar.sthlm@gmail.com>
2021-11-27 13:17:17 +0100

    widget try 1

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 //widget från CoinMC rullande lista
 {/* <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js%22%3E</script>
 <div id="coinmarketcap-widget-marquee" coins="1,1027,2010,6636,3513,5994,5426,52,3783,5805,1082,3220,4172,7080,3890,2496" currency="SEK" theme="light" transparent="false" show-symbol-logo="true"></div> */}
+
+//<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js%22%3E</script>
+//< id="coinmarketcap-widget-marquee" coins="1,1027,2010,6636,3513,5994,5426,52,3783,5805,1082,3220,4172,7080,3890,2496" currency="SEK" theme="light" transparent="false" show-symbol-logo="true">  -->
