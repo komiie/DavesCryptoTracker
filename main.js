01e61fdb7d2cfb70f26fd5408e64dd991160fefb
David Komar <komar.sthlm@gmail.com>
2021-11-27 15:43:49 +0100

    api 2

--- main.js
+++ main.js
@@ -4,3 +4,16 @@
 
 //<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js%22%3E</script>
 //< id="coinmarketcap-widget-marquee" coins="1,1027,2010,6636,3513,5994,5426,52,3783,5805,1082,3220,4172,7080,3890,2496" currency="SEK" theme="light" transparent="false" show-symbol-logo="true">  -->
+
+
+Coingecko
+
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+
+Coinranking:
+API key: coinranking275216714701ea4bafba4bceeaef0aa5a544540c0cd9ae5d  
+
+
+CoinMC:
+2199e89c-42b9-4a0e-a5d6-9cc8ef079721
