b9cc798c2c240e5f2fcaed12f87df940d40b197d
David Komar <komar.sthlm@gmail.com>
2021-11-28 14:38:24 +0100

    edited dev async await function

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 //widget från CoinMC rullande lista
 /* <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js%22%3E</script>
 <div id="coinmarketcap-widget-marquee" coins="1,1027,2010,6636,3513,5994,5426,52,3783,5805,1082,3220,4172,7080,3890,2496" currency="SEK" theme="light" transparent="false" show-symbol-logo="true"></div> */
-
-
 //Coinranking:
 //API key: coinranking275216714701ea4bafba4bceeaef0aa5a544540c0cd9ae5d  
 //CoinMC:
@@ -26,8 +24,13 @@
 //             </tr> 
 //         </table>
 
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false").then(function(response){
-    return response.json()
-    }).then(function(data) {
+//fetch listan för select: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false
+
+let apiLink = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false"
+
+async function loadAPI() { 
+const response = await fetch(apiLink)
+const data =  await response.json()
         console.log(data)        
-    })
+    }
+loadAPI()
