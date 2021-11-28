48f29d8842e48ac8f0fcd4afbae553a8437f01da
David Komar <komar.sthlm@gmail.com>
2021-11-28 11:11:27 +0100

    edit coins

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 
 //Coingecko
 //add these: id, symbol, name, image, current price, market cap, total_supply, 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false
 
 //Coinranking:
 //API key: coinranking275216714701ea4bafba4bceeaef0aa5a544540c0cd9ae5d  
