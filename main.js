8f099a00d563444f6ca93d4680374b9792c6018e
David Komar <komar.sthlm@gmail.com>
2021-11-30 14:05:27 +0100

    deleted old api's

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-//widget från CoinMC rullande lista
-/* <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js%22%3E</script>
-<div id="coinmarketcap-widget-marquee" coins="1,1027,2010,6636,3513,5994,5426,52,3783,5805,1082,3220,4172,7080,3890,2496" currency="SEK" theme="light" transparent="false" show-symbol-logo="true"></div> */
-//Coinranking:
-//API key: coinranking275216714701ea4bafba4bceeaef0aa5a544540c0cd9ae5d  
-//CoinMC:
-//2199e89c-42b9-4a0e-a5d6-9cc8ef079721
-
-//Coingecko seems to work eller? JSON format
-//add these: symbol, name, image, current price, market cap, total_supply, 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false
-
-// static eller inte...
 // return <table> innerHTML =  onclick = loop ?!v `
 
 //             <tr> 
@@ -19,7 +6,6 @@
 //                 <td>{data.name}</td>
 //                 <td>"$"+{data.market_cap}</td>
 //                 <td>{data.current_price}</td>
-//     
 //             </tr> 
 //         </table>`
 
@@ -39,6 +25,7 @@ const getData =  await response.json()
     }
    
 //2. Table function generator
+cryptotable.innerHTML = 
 
 
 //3. Dropdown-list items generator function
